71f9fdf9a46118b0989cc6a0049f24654da6aa99
ErkkiTa <44016792+ErkkiTa@users.noreply.github.com>
2019-12-16 15:11:35 +0200

    Update index.php

--- index.php
+++ index.php
@@ -15,8 +15,45 @@ try {
 } catch (\PDOException $e) {
      throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
-$stmt = $pdo->prepare('SELECT title FROM books WHERE release_date = :year');
-$stmt->execute(['year'=>"2000"]);
-while ($row = $stmt->fetch()){
-    echo $row['title'] . "<br><br>";
+//var_dump($_GET);
+$title = $_GET['title'];
+$year = $_GET['year'];
+$stmt = $pdo->prepare('SELECT * FROM books WHERE title LIKE :title AND release_date =:year');
+$stmt->execute(['title' => '%' . $title . '%' ,'year'=> $year]);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Raamatu otsng</title>
+</head>
+    <style>
+        h1 { color: #ff00ae; }
+    </style>
+<body>
+
+    <h1> Otsinguriba </h1>
+    
+    </form>
+    
+    
+    <form action="index.php" method="GET">
+        <input id="otsing" name="title" type="text" placeholder="Raamatu nimi" value="<?=$title;?>">
+        <input id="otsing" name="year" type="text" placeholder="Raamatu väljalaske auto" value="<?=$year;?>">
+        <input id="submit" type="submit" style="font-face: 'Comic Sans MS'; font-size: larger; color: #ffffff ; background-color: #ff00ae"  value="Search" class="button"/>
+    </form>
+    <ul>
+<?php
+while ($row = $stmt->fetch())
+{
+    echo '<li>' .$row['title'] . "
+    <br>
+    <br>";
 }
+?>
+    </ul>
+
+</body>
+</html>
